d7aff65928e987fbd268d20aca0595598e376ba0
arshinsalim <arshin.salim@hmcts.net>
2022-10-10 12:01:00 +0100

    "pushing git hub project for cat company"

--- src/main/java/com/catscompany/catscompany/CatscompanyApplication.java
+++ src/main/java/com/catscompany/catscompany/CatscompanyApplication.java
@@ -3,15 +3,18 @@ package com.catscompany.catscompany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
+@EnableEurekaClient
 public class CatscompanyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatscompanyApplication.class, args);
 	}
+
 
 }
